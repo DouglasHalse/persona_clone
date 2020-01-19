040c5bdc4232470693926de7d0d0b7ded10b5bb7
Douglas Halse <douglashalse@douglashalse.com>
2020-01-19 16:05:42 +0100

    Added A* test and added attack animation

---
+++ New Unity Project/Assets/Scripts/actorAttack.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class actorAttack : MonoBehaviour
+{
+    public GameObject actor;
+    public GameObject cam;
+    public bool attack_available; //change to private
+    public GameObject target; // ----
+    public GameObject[] temp;
+    private Vector3 target_loc;
+    private Vector3 actor_loc;
+    private Vector3 arc_middle;
+    private GameObject arc_tester;
+    public float t;
+    public bool attacking;
+    private AssetBundle scenes;
+    private Vector3 get_bez_point(float t)
+    {
+        return (1f - t) * ((1f - t) * actor_loc + t * arc_middle) + t * ((1f - t) * arc_middle + t * target_loc);
+    }
+    // Start is called before the first frame update
+    public bool can_attack()
+    {
+        return attack_available;
+    }
+    public GameObject get_target()
+    {
+        return target;
+    }
+    void Start()
+    {
+
+        temp = GameObject.FindGameObjectsWithTag("enemy");
+        target = null;
+        arc_tester = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        t = 0;
+        attacking = false;
+        scenes = AssetBundle.LoadFromFile("Assets/AssetBundles/scenes");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(enemies.Count);
+        temp = GameObject.FindGameObjectsWithTag("enemy");
+        foreach (GameObject i in temp)
+        {
+            //Debug.Log((i.transform.position - actor.transform.position).magnitude);
+            if ((i.transform.position - actor.transform.position).magnitude<10)
+            {
+                //Debug.Log("Enemy near!");
+                attack_available = true;
+                target = i;
+                break;
+            }
+        }
+        if(target && (target.transform.position - actor.transform.position).magnitude > 10)
+        {
+            attack_available = false;
+            target = null;
+        }
+        if(target)
+        {
+            if(!attacking)
+            {
+                target_loc = target.transform.position + Vector3.up;
+                actor_loc = actor.transform.position;
+                arc_middle = 8 * Vector3.up + (actor_loc + target_loc) / 2;
+                attacking = true;
+            }
+            else
+            {
+                actor.transform.position = get_bez_point(t);
+                t = t + 0.01f;
+                if(t > 1f)
+                {
+                    attacking = false;
+                    this.enabled = false;
+                    //SceneManager.LoadScene("battle");
+                    t = 0;
+                }
+            }
+            
+
+            
+            //arc_tester.transform.position = arc_middle;
+        }
+        else
+        {
+            arc_tester.transform.position = Vector3.zero;
+        }
+    }
+}
---
+++ New Unity Project/Assets/grid_maker.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid_maker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int size_x;
+    private int size_z;
+    private int total_nodes;
+    private List<Vector3> positions;
+    private List<GameObject> nodes;
+    private int[,] adjmatrix = new int[10, 10];
+
+    public void set_xz_size(int x, int z)
+    {
+        size_x = x;
+        size_z = z;
+        total_nodes = x * z;
+
+    }
+    void Start()
+    {
+        size_x = 10;
+        size_z = 10;
+        total_nodes = 100;
+        positions = new List<Vector3>();
+        for (int i = 0; i < total_nodes; i++)
+        {
+            positions.Add(new Vector3(i % size_x, 0, i / size_z));
+        }
+        foreach(Vector3 pos in positions)
+            {
+            GameObject temp = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            temp.name = pos.x.ToString() + pos.z.ToString();
+            temp.transform.Rotate(Vector3.right, -90f);
+            temp.transform.localScale = new Vector3(5, 5, 0);
+            temp.transform.position = pos*5f + new Vector3(-22.5f, 0, -22.5f);
+            temp.transform.parent = this.transform;
+            //Instantiate(new GameObject("Sphere")).transform.position = pos;
+            }
+        foreach(Transform child in this.transform)
+        {
+            nodes.Add(child.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
